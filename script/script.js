5f837c706275b54d5a63dabdec9acf21ed5eddba
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-03-14 11:09:56 -0300

    talvez

--- script/script.js
+++ script/script.js
@@ -4,28 +4,24 @@ function imc(){
     const height = document.getElementById("height").value.replace(",",".");
     const p = document.getElementById("p");
     const calc = document.querySelector(".calc");
+
     try {
         if(name.value == "" || weight.value == "" || height.value == "" || p.value == ""){
             alert("Por favor, preencha todos os campos")
         }else{
         let calcImc = eval(weight) / eval(height**2);
         let indiceWeight = ""
-        calcImc = calcImc.toFixed(1)
-        if(calcImc < 18.5){
-            indiceWeight = "Abaixo do peso";
-        }else if(calcImc >=18.5 && calcImc <= 24.9 ){
-            indiceWeight = "Peso normal";
-        }else if(calcImc >=25 && calcImc >= 29.9){
-            indiceWeight = "Sobrepeso";
-        }else{
-            
-        }
+        calcImc = calcImc.toFixed(2)
+        
         p.textContent = `O resultado é :${calcImc}. ${indiceWeight}`
         document.getElementById("weight").value = "";
         document.getElementById("height").value = "";
         document.getElementById("name").value = "";
-    }
+        }
+    
     }catch(e){
         console.log(e)
     }    
-}
+    }
+
+
